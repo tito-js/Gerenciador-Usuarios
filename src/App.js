4a58be4673a7498ac560903afd1b290b8303f951
Gabriel Rocha <titozinnz@gmail.com>
2023-05-19 17:19:24 -0300

    Gerenciador de Usuarios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Listagem from "./Componentes/Listagem"
 
 function App() {
+
+	// Como Funciona os objetos em JavaScript
+	const usuario = {
+		// chave: valor
+		nome: "Conrado",
+		idade: 15,
+		status: true,
+		endereco: {
+			rua: "Rua x",
+			numero: 139,
+			complemneto:"ap 20"
+		},
+		vacinas_covid19:["Pfizer", "Pfizer", "Pfizer", "J&J"]
+	}
+	//console.log(usuario)
+	//console.log(usuario.endereco.rua)
+	//conselo.log( Objects.key(usuario))
+
+	//Como usar um array com objetos dentro
+	const usuarios = [
+		{nome: "Gabriel", status: true},
+		{nome: "Grazi", status: true},
+		{nome: "Luana", status: true},
+		{nome: "Conrado", status:false}
+	]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+
+
+		<div className="d-flex justify-content-around">
+
+			<Listagem usuarios={usuarios} />
+
+			<div className="caixa"> 
+				<h2> Cadastro </h2>
+			</div>
+
+		</div>
+		
+
+      </div>
   );
 }
 
