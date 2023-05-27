703706062212429dd487dc1369b2e6e093ad4262
tito-mov <titozinnz@gmail.com>
2023-05-26 23:05:13 -0300

    Forms Validation
    Validação do formulário, usando JavaScript com as bibliotecas Bootstrap e React-Toastify

--- src/Componentes/Cadastro.js
+++ src/Componentes/Cadastro.js
@@ -1,36 +1,101 @@
 import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Cadastro({usuarios,setUsuarios}){
-
     const [ nome, setNome] = React.useState("");
     const [ status, setStatus] = React.useState(true);
     const [ email, setEmail] = React.useState("");
-    
-    
+  
     function salvar(e){
         e.preventDefault()
+
+        function validaEmail(email){
+            let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return re.test(email)
     
-        const Usuario = {
-            nome: nome, 
-            status: status,
-            email: email
         }
-        //O operador ... se chama spread ou "despejar"
-        setUsuarios([...usuarios, Usuario])
+            if(nome == ""){
+                toast.error('Por favor digite um nome!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    });
+                return false;
+            }
+            
+            if(nome.length <3){
+                toast.error('O nome deve possuir mais que 3 caracteres.', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    });
+                return false;
+            }
+    
+            if(email == ""){
+                toast.error('Por favor, digite um e-mail válido.', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    });
+                return false;
+            }
+    
+            if(!validaEmail(email)){
+                toast.error('Por favor, digite um e-mail válido.', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    });
+                return false;
+            }
+    
+            const Usuario = {
+                nome: nome, 
+                status: status,
+                email: email
+            }
+            //O operador ... se chama spread ou "despejar"
+            setUsuarios([...usuarios, Usuario])
+
+            return true;
     }
 
+    
+    
     return(
         <div className="caixa"> 
 				<h2> Cadastro </h2>
 
-					<form onSubmit={(e) => salvar(e)}>
+					<form id="Forms"onSubmit={(e) => salvar(e)}>
 						<div class="form-floating mb-3">
 							<input onChange={e => setNome(e.target.value)} class="form-control" id="txtNome" placeholder="Nome"/>
 							<label for="txtNome">Nome</label>
 						</div>
 
                         <div class="form-floating mb-3">
-                            <input onChange={e => setEmail(e.target.value)}type="email" class="form-control" id="txtEmail" placeholder="name@example.com"/>
+                            <input onChange={e => setEmail(e.target.value)} class="form-control" id="txtEmail" placeholder="name@example.com"/>
                             <label for="txtEmail">Endereço de Email</label>
                         </div>
 
@@ -40,8 +105,9 @@ function Cadastro({usuarios,setUsuarios}){
 						</div>
 
 						<button type="submit" class="btn btn-primary">Salvar</button>
+                        <ToastContainer/>
 					</form>
-			</div>
+	        </div>
 
     )
 }
